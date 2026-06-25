03378b6ffc0ceebc269a3f8852f1e5979c5eaaaa
Cristian Garcia <cgarciae@google.com>
2026-06-25 16:25:44 -0700

    add to_lojax for named_computation_p
    implements named_computation_p.to_lojax to fix hijax issue
    
    PiperOrigin-RevId: 938253702

--- jax/_src/interpreters/partial_eval.py
+++ jax/_src/interpreters/partial_eval.py
@@ -1986,6 +1986,7 @@ class DynamicJaxprTrace(core.Trace):
                              propagate_source_info=False)
 
     new_jaxpr = convert_constvars_jaxpr(jaxpr)
+    self.frame.is_high |= new_jaxpr.is_high
     if isinstance(call_primitive, core.ClosedCallPrimitive):
       new_jaxpr = close_jaxpr(new_jaxpr)
     new_params = dict(params, call_jaxpr=new_jaxpr)
@@ -2604,3 +2605,12 @@ def raise_lo_outs(hi_avals, lo_outs):
 
 def num_himuts_out(final_qdds):
   return sum(len(a.lo_ty()) for a in final_qdds if a.has_qdd)
+
+
+def _closed_call_to_lojax(*hi_args, call_jaxpr: ClosedJaxpr, **_):
+  from jax._src.custom_derivatives import _lower_and_eval  # pyrefly: ignore[missing-import]
+
+  return _lower_and_eval("closed_call", call_jaxpr, hi_args)
+
+
+core.closed_call_p.to_lojax = _closed_call_to_lojax
